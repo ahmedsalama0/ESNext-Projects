ae4e3823fe51770c049f1f1b8beee34d7040b124
ahmedsalama0 <130906715+ahmedsalama0@users.noreply.github.com>
2025-07-13 02:01:00 +0300

    fixing design issues 1307

--- movie-pulse/home.js
+++ movie-pulse/home.js
@@ -8,8 +8,59 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+const API_KEY = 'd0a03d69b648864175d93fd46a6c9fff';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+const movieContainer = document.querySelector('.movie-container');
 const ActiveButton = document.querySelector('.nav-link .active');
-function init() { }
-function FechData() {
-    return __awaiter(this, void 0, void 0, function* () { });
+function fetchData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch(`${BASE_URL}/discover/movie?api_key=${API_KEY}`
+            //`${BASE_URL}/discover/tv?api_key=${API_KEY}`
+            //`${BASE_URL}/movie/popular?api_key=${API_KEY}`
+            //`${BASE_URL}/movie/changes?api_key=${API_KEY}`
+            );
+            if (!response.ok)
+                throw new Error('Error while fetching data!');
+            const data = yield response.json();
+            if (!data)
+                throw new Error('No data for this category!');
+            console.log(data);
+            data.results.forEach((result) => {
+                const html = `
+        <div class="card" style="width: 18rem;">
+          <img src="${IMAGE_BASE_URL + '/' + (result === null || result === void 0 ? void 0 : result.poster_path)}" class="card-img-top img-fluid   " alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${result.title}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">View Details</a>
+          </div>
+        </div>
+      `;
+                movieContainer.insertAdjacentHTML('afterbegin', html);
+            });
+        }
+        catch (e) {
+            console.error(e);
+        }
+    });
 }
+function init() {
+    fetchData();
+}
+init();
+/*
+ const html: string = `
+        <div class="card" style="width: 18rem;">
+          <img src="${
+            IMAGE_BASE_URL + '/' + result?.poster_path
+          }" class="card-img-top img-fluid   " alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${result.title}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">View Details</a>
+          </div>
+        </div>
+      `;
+*/
--- movie-pulse/home.ts
+++ movie-pulse/home.ts
@@ -1,6 +1,65 @@
+const API_KEY: string = 'd0a03d69b648864175d93fd46a6c9fff';
+const BASE_URL: string = 'https://api.themoviedb.org/3';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+const movieContainer: HTMLDivElement =
+  document.querySelector('.movie-container')!;
+
 const ActiveButton: HTMLButtonElement =
   document.querySelector('.nav-link .active')!;
 
-function init(): void {}
+async function fetchData(): Promise<void> {
+  try {
+    const response: Response = await fetch(
+      `${BASE_URL}/discover/movie?api_key=${API_KEY}`
+      //`${BASE_URL}/discover/tv?api_key=${API_KEY}`
+      //`${BASE_URL}/movie/popular?api_key=${API_KEY}`
+      //`${BASE_URL}/movie/changes?api_key=${API_KEY}`
+    );
 
-async function FechData() {}
+    if (!response.ok) throw new Error('Error while fetching data!');
+
+    const data = await response.json();
+
+    if (!data) throw new Error('No data for this category!');
+    console.log(data);
+
+    data.results.forEach((result: any) => {
+      const html: string = `
+        <div class="card" style="width: 18rem;">
+          <img src="${
+            IMAGE_BASE_URL + '/' + result?.poster_path
+          }" class="card-img-top img-fluid   " alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${result.title}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">View Details</a>
+          </div>
+        </div>
+      `;
+      movieContainer.insertAdjacentHTML('afterbegin', html);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function init(): void {
+  fetchData();
+}
+init();
+
+/*
+ const html: string = `
+        <div class="card" style="width: 18rem;">
+          <img src="${
+            IMAGE_BASE_URL + '/' + result?.poster_path
+          }" class="card-img-top img-fluid   " alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${result.title}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">View Details</a>
+          </div>
+        </div>
+      `;
+*/
